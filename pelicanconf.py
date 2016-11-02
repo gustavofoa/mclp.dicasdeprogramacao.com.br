d7ba1924be6c5717adc44e9300c2d2220f925606
gustavofoa <gustavo.foa@gmail.com>
2016-11-02 10:59:46 -0200

    Config publishing

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,7 +26,7 @@ DEFAULT_PAGINATION = False
 
 # Plugins
 PLUGIN_PATHS = ['./pelican-plugins']
-PLUGINS = ['sitemap', 'minify']
+PLUGINS = ['sitemap']
 
 # Sitemap
 SITEMAP = {
@@ -56,7 +56,6 @@ LP_DESCRIPTION = "Coloque seu e-mail abaixo e receba GRATUITAMENTE o minicurso d
 LP_ACTION = "http://mail.gustavofurtado.com.br/subscribe"
 LP_HIDDEN_FIELDS = '<input type="hidden" name="list" value="oiJK0j8gmAdoxtJtv2pSxA" />'
 TITLEBAR_TEXT = "Receba este minicurso de lógica de programação GRÁTIS!"
-GOOGLE_ANALYTICS = "UA-39997045-2"
 TOTAL_LESSONS = 10
 SHARE_ON_TWITTER = "http://ctt.ec/4Rq9f"
 SHARE_ON_FACEBOOK = "http://www.facebook.com/sharer.php?u=http://www.dicasdeprogramacao.com.br/minicurso-logica-de-programacao/"
