dbbf7188f39b28ab6c7a6c030838b388da3442c2
Gustavo Furtado de Oliveira Alves <gustavo.foa@gmail.com>
2018-06-20 18:04:30 -0300

    minify and do not detect encoding.

--- publishconf.py
+++ publishconf.py
@@ -26,3 +26,8 @@ PLUGINS = ['sitemap', 'minify']
 
 #DISQUS_SITENAME = ""
 GOOGLE_ANALYTICS = "UA-39997045-2"
+MINIFY = {
+ 'remove_comments': True,
+ 'remove_all_empty_space': True,
+ 'remove_optional_attribute_quotes': False
+}
